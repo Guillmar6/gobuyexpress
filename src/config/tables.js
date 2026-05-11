53512ae1a7136e62cb09768298b7830ad1563790
Guillmar Reyes <pton8169@gmail.com>
2026-05-11 16:29:42 +0800

    Fix

--- src/config/tables.js
+++ src/config/tables.js
@@ -2,7 +2,7 @@ const pool = require("./db");
 
 async function userTable(req, res, next) {
     await pool.query(
-        "CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, password VARCHAR NOT NULL, phone_number INT8, email VARCHAR, gender VARCHAR, birthday TEXT)"
+        "CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, name VARCHAR NOT NULL, password VARCHAR NOT NULL, phone_number TEXT, email VARCHAR, gender VARCHAR, birthday TEXT)"
     );
     next();
 }
